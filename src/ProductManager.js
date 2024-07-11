94853907f8edb8af7edb853397989a49fe48f3ad
Jesus <gimenezjesus18@gmail.com>
2024-07-11 14:34:19 -0400

    segundo desafio

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,19 +1,27 @@
+const fs = require('fs');
+
 // Creamos la primera clase
 class ProductManager{
-    constructor(){
+    constructor(path){
         this.products = [];
+        this.path = path
         this.id = 0;
     }
 
-    getProduct(){
-        return this.products
+    async getProduct(){
+        // Leer el archivo de productos
+        const listProducts = await fs.promises.readFile(this.path, 'utf-8');
+
+        // Convertir el contenido del archivo JSON a un array de productos
+        const products = JSON.parse(listProducts);
+        return products;
     }
 
-    addProduct({ title, description, price, thumbnail, code, stock }){
+    async addProduct({ title, description, price, thumbnail, code, stock }){
         
         // Para agregar los productos debe llenar todo los campos
         if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log("Todos los campos son obligatorios.");
+            console.error("Todos los campos son obligatorios.");
             return;
         }
 
@@ -39,12 +47,54 @@ class ProductManager{
 
         // metodo para agregar el producto
         this.products.push(newProduct);
+
+        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
+
         console.log("Producto agregado con exito", newProduct);
     }
 
-    getProductById(id){
+    
+    async updateProduct(id, title, description, price, thumbnail, code, stock){
+        //para actualizar un producto debe ingresar los todos los datos
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Todos los campos son obligatorios.");
+            return;
+        }
+        else{
+            const data = await this.getProduct();
+            const existingProduct = data.find((element) => element.code === code);
+            if(existingProduct){
+                console.error(`El producto con el código ${code} ya existe.`);
+                return;
+            }
+            else{
+                const listProduct = await this.getProduct();
+                const newListProduct = listProduct.map((element) =>{
+                    if (element.id===id){
+                        const updateProduct = {
+                            ...element,
+                            title,description,price,thumbnail,code,stock
+                        }
+                        return updateProduct
+                    }
+                    else{
+                        return product;
+                    }
+                })
+                await fs.promises.writeFile(this.path,JSON.stringify(newListProduct,null,"\t"))
+            }
+        }
+    }
+
+    async deleteProduct(id){
+        const data = await this.getProduct();
+        const product = data.filter(element => element.id === id)
+        await fs.promises.writeFile(yhis.path,JSON.stringify(product,null,"\t"))
+    }
+    
+    async getProductById(id){
         // Metodo para obtener el producto por id
-        const product = this.products.find(product => product.id === id);
+        const product = await this.products.find(product => product.id === id);
     if (!product) {
         console.log('Producto no encontrado.');
     }
@@ -53,25 +103,98 @@ class ProductManager{
 }
 
 // Instanciamos
-const PM = new ProductManager();
+const run = async () =>{
+    const PM = new ProductManager("products.json");
+    
+    PM.addProduct({
+        title: "product1",
+        description: "description1",
+        price: 1000,
+        thumbnail: "path",
+        code: "code1",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product2",
+        description: "description2",
+        price: 1000,
+        thumbnail: "path",
+        code: "code2",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product3",
+        description: "description3",
+        price: 1000,
+        thumbnail: "path",
+        code: "code3",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product4",
+        description: "description4",
+        price: 1000,
+        thumbnail: "path",
+        code: "code4",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product5",
+        description: "description5",
+        price: 1000,
+        thumbnail: "path",
+        code: "code5",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product6",
+        description: "description6",
+        price: 1000,
+        thumbnail: "path",
+        code: "code6",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product7",
+        description: "description7",
+        price: 1000,
+        thumbnail: "path",
+        code: "code7",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product8",
+        description: "description8",
+        price: 1000,
+        thumbnail: "path",
+        code: "code8",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product9",
+        description: "description9",
+        price: 1000,
+        thumbnail: "path",
+        code: "code9",
+        stock: 10
+    });
+    
+    PM.addProduct({
+        title: "product10",
+        description: "description10",
+        price: 1000,
+        thumbnail: "path",
+        code: "code10",
+        stock: 10
+    });
+}
 
-PM.addProduct({
-    title: "product1",
-    description: "description1",
-    price: 1000,
-    thumbnail: "path",
-    code: "code1",
-    stock: 10
-});
-
-PM.addProduct({
-    title: "product2",
-    description: "description2",
-    price: 1000,
-    thumbnail: "path",
-    code: "code2",
-    stock: 10
-});
-
-console.log(PM.getProduct);
-console.log(PM.getProduct);
+run();
